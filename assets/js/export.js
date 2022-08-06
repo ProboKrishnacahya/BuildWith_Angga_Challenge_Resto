858f8c7ed700f49b48aea56ea8aec2ce2656b7a5
Probo Krishnacahya <probo.krishnacahya@gmail.com>
2022-08-06 16:56:00 +0700

    done

---
+++ assets/js/export.js
@@ -0,0 +1,8 @@
+// * Start of Export Animate On Scroll Library's initialization.
+export let initAOS =
+    AOS.init({
+        duration: 750 // Set the duration of animation to 750ms
+    });
+
+export let refreshHardAOS = AOS.refreshHard(); // Reinit `array` with AOS elements and trigger refresh to called on `Document Object Model` changes that are related to aos elements
+// * End of Export Animate On Scroll Library's initialization.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
+import {initAOS, refreshHardAOS} from './export.js'
+
 //* Start of Website Preloading
 const fadeOut = () => {
-    const preloaderWrapper = document.querySelector('.preloader');
-    preloaderWrapper.classList.add('fade');
+    const preloaderWrapper = document.querySelector('#preloader'); // Returns the class of `preloader` within the document that matches the specified or group of selectors
+    preloaderWrapper.classList.add('fade'); // Returns a live `DOMTokenList` collection of the `class` attributes of the element to manipulate the class list
 }
 
-window.addEventListener('load', fadeOut);
+window.addEventListener('load', fadeOut); // Triggered when the whole page has loaded to show the `fadeOut`
 //* End of Website Preloading
-// $(function () {
-//     AOS.init();
-//     AOS.refreshHard();
-// });
-// window.addEventListener('load', AOS.refresh);
+
+// * Start of writes/ logs a message to the console for testing Animate On Scroll Library
+console.log(initAOS, refreshHardAOS);
+// * End of writes/ logs a message to the console for testing Animate On Scroll Library
+
 //* Start of Restaurant Photo (Image Slider)
-// Returns the first element within the document that matches the specified or group of selectors.
+// Returns the class of `slider` and `slider-inner` within the document that matches the specified or group of selectors
 let outerSlider = document.querySelector(".slider");
 let innerSlider = document.querySelector(".slider-inner");
 
@@ -20,45 +22,44 @@ let pressed = false;
 let startX;
 let horizontal;
 
-// Appends an event listener for events whose type attribute value is type by using mousemove event that fired at an element when a cursor touches the target element's area.
-// Triggered at an element when a pointing device button is pressed while the pointer is inside the element.
+// Appends an event listener for events whose type attribute value is type by using mousemove event that fired at an element when a cursor touches the target element's area
+// Triggered at an element when a pointing device button is pressed while the pointer is inside the element
 outerSlider.addEventListener("mousedown", (e) => {
     pressed = true;
     startX = e.offsetX - innerSlider.offsetLeft;
     outerSlider.style.cursor = "grabbing";
 });
 
-// Triggered at an element when a cursor is initially moved so that its hotspot is within the element at which the event was fired.
+// Triggered at an element when a cursor is initially moved so that its hotspot is within the element at which the event was fired
 outerSlider.addEventListener("mouseenter", () => {
     outerSlider.style.cursor = "grab";
 });
 
-// Triggered at an element when a cursor is moved out of it.
+// Triggered at an element when a cursor is moved out of it
 outerSlider.addEventListener("mouseleave", () => {
     outerSlider.style.cursor = "default";
 });
 
-// Triggered at an element when a button on a pointing device is released while the pointer is located inside it.
+// Triggered at an element when a button on a pointing device is released while the pointer is located inside it
 outerSlider.addEventListener("mouseup", () => {
     outerSlider.style.cursor = "grab";
     pressed = false;
 });
 
-// Triggered at an element when a cursor is moved while the cursor's hotspot is inside it.
+// Triggered at an element when a cursor is moved while the cursor's hotspot is inside it
 outerSlider.addEventListener("mousemove", (e) => {
     if (!pressed) return;
     e.preventDefault(); // Stop the default action of an element from happening
 
-    horizontal = e.offsetX;
+    horizontal = e.offsetX; // Returns the x-coordinate of the cursor that relative to the target element.
 
     innerSlider.style.left = `${horizontal - startX}px`;
 
-    boundaryDetection();
+    boundaryDetection(); // Checking the edge of element area
 });
 
-// Block-scoped variable declaration for checking the edge of element area
 const boundaryDetection = () => {
-    // Returns a `DOMRect` object providing information about the size of an element and its position relative to the viewport.
+    // Returns a `DOMRect` object providing information about the size of an element and its position relative to the viewport
     let outer = outerSlider.getBoundingClientRect();
     let inner = innerSlider.getBoundingClientRect();
 
